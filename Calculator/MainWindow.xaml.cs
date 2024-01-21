698b3037f05251539cf9d5544d4ec3edd197ca5c
Эрик Губайдуллин <gybaidyllin.erik@gmail.com>
2024-01-21 21:02:01 +0300

    calculator finished..

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -21,16 +21,14 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
-        private object textBox;
+        string field;
+        double num1, num2;
+        string action;
+        double result;
 
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            int numb;
         }
 
         private void Button_Click_0(object sender, RoutedEventArgs e)
@@ -40,82 +38,113 @@ namespace Calculator
        
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-
+            textbox.Text += "1";
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
-
+            textbox.Text += "2";
         }
 
         private void Button_Click_3(object sender, RoutedEventArgs e)
         {
-
+            textbox.Text += "3";
         }
 
         private void Button_Click_4(object sender, RoutedEventArgs e)
         {
-
+            textbox.Text += "4";
         }
 
         private void Button_Click_5(object sender, RoutedEventArgs e)
         {
-
+            textbox.Text += "5";
         }
 
         private void Button_Click_6(object sender, RoutedEventArgs e)
         {
-
+            textbox.Text += "6";
         }
 
         private void Button_Click_7(object sender, RoutedEventArgs e)
         {
-
+            textbox.Text += "7";
         }
 
         private void Button_Click_8(object sender, RoutedEventArgs e)
         {
-
+            textbox.Text += "8";
         }
 
         private void Button_Click_9(object sender, RoutedEventArgs e)
         {
-
+            textbox.Text += "9";
         }
 
         private void Button_Click_Clear(object sender, RoutedEventArgs e)
         {
-
+            textbox.Text = "";
+            num1 = 0;
+            num2 = 0;
+            result = 0;
+            action = "";
         }
 
         private void Button_Click_Equal(object sender, RoutedEventArgs e)
         {
+            num2 = double.Parse(textbox.Text);
+            switch (action)
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "×":
+                    result = num1 * num2;
+                    break;
+                case "÷":
+                    result = num1 / num2;
+                    break;
+            }
 
+            textbox.Text = Convert.ToString(result);
         }
 
         private void Button_Click_Addition(object sender, RoutedEventArgs e)
         {
-
+            action = "+";
+            num1 = double.Parse(textbox.Text);
+            textbox.Text = "";
         }
 
         private void Button_Click_Substraction(object sender, RoutedEventArgs e)
         {
-
+            action = "-";
+            num1 = double.Parse(textbox.Text);
+            textbox.Text = "";
         }
 
         private void Button_Click_Multiplication(object sender, RoutedEventArgs e)
         {
-
+            action = "×";
+            num1 = double.Parse(textbox.Text);
+            textbox.Text = "";
         }
 
         private void Button_Click_Division(object sender, RoutedEventArgs e)
         {
-
+            action = "÷";
+            num1 = double.Parse(textbox.Text);
+            textbox.Text = "";
         }
 
         private void Button_Click_Delete(object sender, RoutedEventArgs e)
         {
-
+            field = textbox.Text;
+            field = field.Remove(field.Length - 1);
+            textbox.Text = field;
         }
     }
 }
